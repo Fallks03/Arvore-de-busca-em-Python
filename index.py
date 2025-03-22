e253b35133a3943907eda1745a45d3262f80c8d0
Fallks03 <140653664+Fallks03@users.noreply.github.com>
2025-03-21 21:14:11 -0300

    Correção das ligações no grafo
    algumas ligações estavam erradas no grafo

--- index.py
+++ index.py
@@ -1,26 +1,27 @@
-import functions
 
 grafo = {
-    "Campinas": {"Paulínia": 23, "Monte Mor": 22, "Indaiatuba": 20},
-    "Paulínia": {"Americana": 22, "Campinas": 23},
-    "Americana": {"Paulínia": 22, "Sumaré": 18},
-    "Sumaré": {"Americana": 18, "Monte Mor": 15},
-    "Monte Mor": {"Campinas": 22, "Sumaré": 15, "Capivari": 25, "Indaiatuba": 20},
-    "Capivari": {"Monte Mor": 25, "Tietê": 30, "Piracicaba": 32},
-    "Piracicaba": {"Capivari": 32, "Americana": 30},
-    "Indaiatuba": {"Monte Mor": 20, "Salto": 20},
-    "Salto": {"Indaiatuba": 20, "Itu": 10, "Porto Feliz": 12},
-    "Itu": {"Salto": 10, "Sorocaba": 8},
+    "Campinas": {"Paulínia": 25, "Sumaré": 23, "Monte Mor": 22, "Indaiatuba": 20},
+    "Paulínia": {"Americana": 22, "Campinas": 25},
+    "Americana": {"Paulínia": 22, "Sumaré": 18, "Piracicaba": 30},
+    "Sumaré": {"Americana": 18, "Campinas": 23},
+    "Monte Mor": {"Campinas": 22, "Capivari": 15},
+    "Capivari": {"Monte Mor": 15, "Tietê": 30, "Piracicaba": 32, "Salto": 25},
+    "Piracicaba": {"Capivari": 32, "Americana": 30, "Tietê": 35},
+    "Indaiatuba": {"Campinas": 20, "Salto": 20},
+    "Salto": {"Indaiatuba": 20, "Itu": 10, "Capivari": 25},
+    "Itu": {"Salto": 10, "Sorocaba": 8, "Porto Feliz": 12},
     "Sorocaba": {"Itu": 8, "Boituva": 23},
-    "Boituva": {"Sorocaba": 23, "Porto Feliz": 12, "Tatuí": 12},
-    "Porto Feliz": {"Boituva": 12, "Salto": 12, "Tietê": 30},
-    "Tietê": {"Capivari": 30, "Porto Feliz": 30, "Tatuí": 25},
-    "Tatuí": {"Boituva": 12, "Tietê": 25, "Sorocaba": 17}
+    "Boituva": {"Sorocaba": 23, "Porto Feliz": 12, "Tatuí": 17},
+    "Porto Feliz": {"Boituva": 12, "Tietê": 30, "Itu": 12},
+    "Tietê": {"Capivari": 30, "Porto Feliz": 30, "Tatuí": 25, "Piracicaba": 35},
+    "Tatuí": {"Boituva": 17, "Tietê": 25}
 }
 
 arvore = {}
 
-quantCidades = len(grafo)
+#organiza os valores de cada chave do grafo do menor ao maior dentro do dicionario
+for chave in grafo:
+    grafo[chave] = dict(sorted(grafo[chave].items(), key = lambda item: item[1]))
 
 origem = input("Qual a cidade de origem?\n> ")
 if not(origem in grafo):
@@ -41,9 +42,9 @@ if tipoAlgoritmo == 'BFS':
     cidadeAtual = origem
     indiceFila = 0
     
-    while len(fila) < quantCidades:
+    while len(fila) < len(grafo):
         arvore.update({cidadeAtual: {}})
-        print(arvore)
+        
         for cidade, peso in grafo[cidadeAtual].items():
             if cidade not in fila:
                 fila.append(cidade)
@@ -52,6 +53,8 @@ if tipoAlgoritmo == 'BFS':
         
         indiceFila += 1
         cidadeAtual = fila[indiceFila]
+    print(arvore)
+    print(fila)
 
 
     
