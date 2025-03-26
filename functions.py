dbecae317a4cd0b43645400f0a8972caef23dc07
Felipe Soares <140653664+Fallks03@users.noreply.github.com>
2025-03-26 11:48:04 -0300

    adicionado a função para achar menor caminho da arvore

--- functions.py
+++ functions.py
@@ -11,3 +11,26 @@ def escreverFila(fila):
     for i in fila:
         print(i, end=", ") if i != fila[-1] else print(i, end="")
     print("}")
+
+def menorCaminho(arvore, destino):
+    cidadeAtual = destino
+    cidadeOrigem = next(iter(arvore.keys())) #pega o primeiro item da arvore
+    menorCaminho = [destino]
+    txtCaminho = ''
+    pesoCaminho = 0
+    while cidadeAtual != cidadeOrigem:
+        
+        for cidade, conexoes in arvore.items():
+            if cidadeAtual in conexoes:
+                menorCaminho.append(cidade)
+                for c, peso in arvore[cidade].items():
+                    if c == cidadeAtual:
+                        pesoCaminho += peso
+                        
+                cidadeAtual = cidade
+                break
+            
+    for i in range(1, len(menorCaminho)+1):
+        txtCaminho += f' -> {menorCaminho[-i]}' if i != 1 else f'{menorCaminho[-i]}'
+        if menorCaminho[-i] == destino:
+            return f'{txtCaminho} {pesoCaminho}'
