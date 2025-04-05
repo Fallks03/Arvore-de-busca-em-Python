1a7cc4eae0d93b3381503592d3ed38ce3c926345
Felipe Soares <felipesilvasoares003@gmail.com>
2025-04-05 15:38:42 -0300

    coloquei as logicas dentro de funções

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import copy
+
 def organizarGrafo(grafo):
     for chave in grafo:
         grafo[chave] = dict(sorted(grafo[chave].items(), key = lambda item: item[1]))
@@ -17,6 +19,82 @@ def buscaBfs(grafo, origem, arvore = {}):
                 
         
         indiceFila += 1
+    return (arvore, fila)
+
+def buscaDfs(grafo, origem, arvore = {}):
+    fila = [origem]
+    visitados = set()
+    visitados.add(origem)
+    indiceFila = -1
+
+    while len(arvore) < len(grafo):
+        cidadeAtual = fila[indiceFila]
+        indiceFila -= 1
+        if cidadeAtual not in arvore:
+            arvore.update({cidadeAtual: {}})
+        
+        for cidade, peso in grafo[cidadeAtual].items():
+            if cidade not in visitados:
+                visitados.add(cidade)
+                arvore[cidadeAtual].update({cidade: peso})
+                fila.append(cidade)
+                indiceFila = -1
+                break
+    return (arvore, fila)
+
+def buscaKruskal(GRAFO, origem, arvore = {}):
+    grafo = copy.deepcopy(GRAFO) #faz uma cópia do grafo original para não alterar o mesmo
+    menorAresta = None
+
+    for cidade in grafo.keys():
+        arvore.update({cidade: {}}) #inicializa a arvore com as cidades do grafo
+
+    i = 0
+    for cidade in grafo:
+        grafo[cidade].update({'tag': {chr(65 + i)}}) #adiciona uma tag a cada cidade
+        i += 1
+
+    fila = [None] * (len(grafo) - 1) #o grafo tem n-1 arestas
+    i = 0 
+    while i < len(grafo) - 1: 
+
+        menorAresta = None
+        #percorre todas as cidades do grafo procurando a menor conexão
+        for cidade in grafo: 
+            for conexao, peso in grafo[cidade].items():
+                if conexao == 'tag':
+                    continue
+
+                if menorAresta == None or peso < menorAresta[2]:
+                    if ((cidade, conexao, peso) not in fila 
+                        and (conexao, cidade, peso) not in fila):
+
+                        #verifica se as cidades não tem a mesma tag
+                        if grafo[cidade]['tag'].isdisjoint(grafo[conexao]['tag']): 
+                            fila[i] = (cidade, conexao, peso)
+                            menorAresta = (cidade, conexao, peso)
+                    else:
+                        continue
+        
+        if menorAresta == None:
+            continue
+
+        #deixa as duas cidades com tags iguais
+        grafo[menorAresta[1]]['tag'].update(grafo[menorAresta[0]]['tag']) 
+        grafo[menorAresta[0]]['tag'].update(grafo[menorAresta[1]]['tag'])
+        
+        #adiciona a nova tag a todos os vizinhos da cidade (apenas se já foram conectadas)
+        for cidade in grafo:
+            if not grafo[cidade]['tag'].isdisjoint(grafo[menorAresta[0]]['tag']) or not grafo[cidade]['tag'].isdisjoint(grafo[menorAresta[1]]['tag']):
+                grafo[cidade]['tag'].update(grafo[menorAresta[0]]['tag'])
+                grafo[cidade]['tag'].update(grafo[menorAresta[1]]['tag'])
+        
+        arvore[menorAresta[0]].update({menorAresta[1]: menorAresta[2]})
+        arvore[menorAresta[1]].update({menorAresta[0]: menorAresta[2]})
+
+        i += 1
+    #organiza a arvore do menor ao maior
+    arvore, _ = buscaBfs(arvore, origem)
     return (arvore, fila)
 
 def escreverArvore(arvore, nivel=1, cidade=None):
--- index.py
+++ index.py
@@ -1,4 +1,8 @@
-from functions import organizarGrafo, escreverFila, escreverArvore, menorCaminho, pesoTotalArvore, buscaBfs
+from functions import (
+    organizarGrafo, escreverFila, escreverArvore,
+    menorCaminho, pesoTotalArvore, 
+    buscaBfs, buscaDfs, buscaKruskal
+)
 import copy
 
 GRAFO = {
@@ -33,6 +37,9 @@ while True:
     fila = None #declaração da fila
 
     origem = input("\nQual a cidade de origem?\n> ").capitalize() #capitalize transforma a primeira letra em maiúscula e o resto em minúscula
+    if origem == 'Sair':
+        print("Saindo...")
+        break
     if not(origem in GRAFO):
         print("Esta cidade não existe no grafo!")
         continue
@@ -58,79 +65,10 @@ while True:
         arvore, fila = buscaBfs(GRAFO, origem) 
         
     elif tipoAlgoritmo in ('DFS', '2'):
-        fila = [origem]
-        visitados = set()
-        visitados.add(origem)
-        indiceFila = -1
-
-        while len(arvore) < len(GRAFO):
-            cidadeAtual = fila[indiceFila]
-            indiceFila -= 1
-            if cidadeAtual not in arvore:
-                arvore.update({cidadeAtual: {}})
-            
-            for cidade, peso in GRAFO[cidadeAtual].items():
-                if cidade not in visitados:
-                    visitados.add(cidade)
-                    arvore[cidadeAtual].update({cidade: peso})
-                    fila.append(cidade)
-                    indiceFila = -1
-                    break
+        arvore, fila = buscaDfs(GRAFO, origem)
 
     elif tipoAlgoritmo in ('KRUSKAL', '3'):
-        grafo = copy.deepcopy(GRAFO) #faz uma cópia do grafo original para não alterar o mesmo
-        menorAresta = None
-
-        for cidade in grafo.keys():
-            arvore.update({cidade: {}}) #inicializa a arvore com as cidades do grafo
-
-        i = 0
-        for cidade in grafo:
-            grafo[cidade].update({'tag': {chr(65 + i)}}) #adiciona uma tag a cada cidade
-            i += 1
-
-        fila = [None] * (len(grafo) * 2) 
-        i = 0 
-        while i < len(grafo) - 1: 
-
-            menorAresta = None
-            #percorre todas as cidades do grafo procurando a menor conexão
-            for cidade in grafo: 
-                for conexao, peso in grafo[cidade].items():
-                    if conexao == 'tag':
-                        continue
-
-                    if menorAresta == None or peso < menorAresta[2]:
-                        if ((cidade, conexao, peso) not in fila 
-                            and (conexao, cidade, peso) not in fila):
-
-                            #verifica se as cidades não tem a mesma tag
-                            if grafo[cidade]['tag'].isdisjoint(grafo[conexao]['tag']): 
-                                fila[i] = (cidade, conexao, peso)
-                                menorAresta = (cidade, conexao, peso)
-                        else:
-                            continue
-            
-            if menorAresta == None:
-                continue
-
-            #deixa as duas cidades com tags iguais
-            grafo[menorAresta[1]]['tag'].update(grafo[menorAresta[0]]['tag']) 
-            grafo[menorAresta[0]]['tag'].update(grafo[menorAresta[1]]['tag'])
-            
-            #adiciona a nova tag a todos os vizinhos da cidade (apenas se já foram conectadas)
-            for cidade in grafo:
-                if not grafo[cidade]['tag'].isdisjoint(grafo[menorAresta[0]]['tag']) or not grafo[cidade]['tag'].isdisjoint(grafo[menorAresta[1]]['tag']):
-                    grafo[cidade]['tag'].update(grafo[menorAresta[0]]['tag'])
-                    grafo[cidade]['tag'].update(grafo[menorAresta[1]]['tag'])
-            
-
-            arvore[menorAresta[0]].update({menorAresta[1]: menorAresta[2]})
-            arvore[menorAresta[1]].update({menorAresta[0]: menorAresta[2]})
-
-            i += 1
-        arvore, _ = buscaBfs(arvore, origem)
-        #organiza a arvore do menor ao maior
+        arvore, fila = buscaKruskal(GRAFO, origem)
     
     print("\nFila:\n")
     escreverFila(fila, tipoAlgoritmo)
